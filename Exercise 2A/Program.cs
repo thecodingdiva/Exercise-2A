4a43086a8e39ab19e61fa21cec8d8bada575e5f6
Joanne <thecodingdiva@gmail.com>
2018-03-28 18:34:46 -0400

    Console app that accepts 10 numbers and reports the sum.

--- Exercise 2A/Program.cs
+++ Exercise 2A/Program.cs
@@ -16,20 +16,44 @@ namespace Exercise_2A
         
         public class SumScores
         {
-            public void Add()
+            public double Add()
             {
-                Console.WriteLine("Please enter a number between 0 - 100: ");
-                double x1 = double.Parse(Console.ReadLine());
-                if (x1 < 0 || x1 > 100)
-                {
-                    Console.WriteLine("Number must be between 0 - 100. Please enter number: ");
-                    double.Parse(Console.ReadLine());
-                }
-                else
-                {
-                    x1 = double.Parse(Console.ReadLine());
-                }
+                double x1, x2, x3, x4, x5, x6, x7, x8, x9, x10;
+                Console.WriteLine("Please enter a number between 0 - 100. 1: ");
+                x1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 2: ");
+                x2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 3: ");
+                x3 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 4: ");
+                x4 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 5: ");
+                x5 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 6: ");
+                x6 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 7: ");
+                x7 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 8: ");
+                x8 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 9: ");
+                x9 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number between 0 - 100. 10: ");
+                x10 = double.Parse(Console.ReadLine());
+
+                double y = x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+                Console.WriteLine("The sum of your scores is: " + y);
+                return y;
             }
         }
+        }
     }
-}
+
