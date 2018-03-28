86bce11c6d4a1910a4cbf9e6c4477f5c0c2ed1a7
Joanne <thecodingdiva@gmail.com>
2018-03-28 15:48:45 -0400

    Add project files.

---
+++ Exercise 2A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+        public class SumScores
+        {
+            public void add(double y)
+            {
+                Console.WriteLine("Please enter a number between 0 - 100: ");
+                double x1 = double.Parse(Console.ReadLine();
+                if (x1 < 0 || x1 > 100)
+                {
+                    Console.WriteLine("Number must be between 0 - 100. Please enter number: ");
+                }
+            }
+        }
+    }
+}
